82a362c3a228ed00935c0e0ef809848e09925307
Eripron <dk5051@naver.com>
2021-11-16 08:41:53 +0900

    Modity CameraContoller
    .

--- Assets/Scripts/Controller/CameraControl/CameraController.cs
+++ Assets/Scripts/Controller/CameraControl/CameraController.cs
@@ -81,22 +81,10 @@ public class CameraController : MonoBehaviour
 
     Vector2 input;
 
+
     void LateUpdate()
     {
-        // new rotate
-
-        input.x = Input.GetAxis("Mouse X") * turnSpeed;
-        input.y = Input.GetAxis("Mouse Y") * turnSpeed;
-
-        if(input.magnitude != 0.0f)
-        {
-            Quaternion rotation = target.rotation;
-            rotation.eulerAngles = new Vector3(rotation.eulerAngles.x + input.y, rotation.eulerAngles.y + input.x, rotation.eulerAngles.z);
-            target.rotation = rotation;
-        }
-
-        transform.LookAt(target);
-
+        OriginRotate();
     }
 
     // 원래 회전방식 
@@ -108,7 +96,26 @@ public class CameraController : MonoBehaviour
             float y = Input.GetAxis("Mouse Y") * turnSpeed;
 
             Quaternion rotateX = Quaternion.AngleAxis(x, Vector3.up);
-            Quaternion rotateY = Quaternion.AngleAxis(y, Vector3.left);
+            Quaternion rotateY;
+
+            // 회전 축 정하기 
+            float camEulerY = transform.eulerAngles.y;
+            if (camEulerY > 45.0f && camEulerY <= 135.0f)
+            {
+                rotateY = Quaternion.AngleAxis(y, Vector3.forward);
+            }
+            else if (camEulerY > 135.0f && camEulerY <= 225.0f)
+            {
+                rotateY = Quaternion.AngleAxis(y, Vector3.right);
+            }
+            else if (camEulerY > 225.0f && camEulerY <= 315.0f)
+            {
+                rotateY = Quaternion.AngleAxis(y, Vector3.back);
+            }
+            else
+            {
+                rotateY = Quaternion.AngleAxis(y, Vector3.left);
+            }
 
             camPos = rotateX * rotateY * camPos;
         }
@@ -121,21 +128,23 @@ public class CameraController : MonoBehaviour
         if (Physics.Raycast(target.position, dir, out hitted, cameraDistance))
         {
             if (hitted.transform.gameObject.layer == (int)LAYER.LAYER_GROUND)
-                finalPos = hitted.point - (dir * 1.5f);
+                finalPos = hitted.point - (dir * 1.2f);
         }
 
         finalPos.y = Mathf.Clamp(finalPos.y, target.position.y - limitDownHeight, target.position.y + limitUpHeight);
 
         transform.position = ClampBoundary(finalPos);
+
         transform.LookAt(target);
     }
 
 
     void SetCameraDistance(float _distance)
     {
+        camPos = new Vector3(0f, 0f, _distance);
         cameraDistance = Mathf.Abs(_distance);
-        camPos = new Vector3(0f, 0f, cameraDistance);
     }
+
     Vector3 ClampBoundary(Vector3 vector)
     {
         vector.y = Mathf.Clamp(vector.y, bottomY + boundaryBottom, limitUpHeight);
