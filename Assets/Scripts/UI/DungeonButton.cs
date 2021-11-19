0c40e1f3732fa42d3cb7987d7811fbfb309a786d
Eripron <dk5051@naver.com>
2021-11-19 14:46:30 +0900

    DungeonInfoUI.
    ..

---
+++ Assets/Scripts/UI/DungeonButton.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DungeonButton : MonoBehaviour
+{
+    [SerializeField] DungeonDetail datailWindow;
+
+    [SerializeField] Sprite bossSprite;
+    [SerializeField] Sprite goalSprite;
+
+    [SerializeField] string dungeonName;
+    [SerializeField] string goal;
+    [SerializeField] string story;
+
+
+    public string DunName => dungeonName;
+    public string Goal => goal;
+    public string Story => story;
+
+    public Sprite BossSprite => bossSprite;
+    public Sprite GoalSprite => goalSprite;
+
+    public void OnDetailWindow()
+    {
+        datailWindow.OnDetailDungeonUI(this);
+    }
+
+
+}
---
+++ Assets/Scripts/UI/DungeonButtonWindow.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DungeonButtonWindow : MonoBehaviour
+{
+    /// <summary>
+    /// 0 - normal
+    /// 1 - highlight
+    /// 2 - selected
+    /// </summary>
+    [SerializeField] Color[] buttonColor;
+
+    [SerializeField] Button[] dungeonButton;
+
+
+
+
+
+
+
+
+
+}
